449a63fa6d2ad8c12e2ab711f130072fedaaeeae
apoorvam <apoorva_rarefriend@yahoo.com>
2016-09-10 00:31:21 +0530

    Adding speaker bio, photo under details

---
+++ js/toggle.js
@@ -0,0 +1,24 @@
+function toggle() {
+    $("#agenda .row.talk").click(function() {
+        var isHidden = $(this).find('.details').hasClass('hidden');
+        $('#agenda .row.talk .details').each(function() {
+            if (!$(this).hasClass('hidden')) {
+                $(this).addClass('hidden');
+            }
+        });
+        $("#agenda .row.talk .summary .read-more").each(function() {
+            $(this).text("Read more...");
+        });
+        var readMore = $(this).find('.read-more');
+        if (isHidden) {
+            $(this).find('.details').removeClass('hidden');
+            readMore.text("Less");
+        } else {
+            $(this).find('.details').addClass('hidden');
+            readMore.text("Read more...");
+        }
+    });
+}
+$(function() {
+    toggle();
+});
