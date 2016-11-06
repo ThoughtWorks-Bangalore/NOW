ebb4316cc0c5bd8240b77850872168bc930df976
apoorvam <apoorva_rarefriend@yahoo.com>
2016-11-06 15:32:51 +0530

    Adding generated redesigned event pages

--- js/ticker.js
+++ js/ticker.js
@@ -6,38 +6,38 @@ var targetTS = targetDate.getTime(),
     $tick = document.getElementById("tick"),
     $tock = document.getElementById("tock"),
     $tack = document.getElementById("tack"),
-    $sElements = [$tick, $tock, $tack],
-    $sClasses = ["tick", "tock", "tack"],
+    $sElements = [ $tick, $tock, $tack ],
+    $sClasses = [ "tick", "tock", "tack" ],
     countdownTimer = null;
 
 function updateCountDown() {
-    var currentDate = new Date().getTime();
-    var secondsLeft = (targetTS - currentDate) / 1000;
-    if (secondsLeft < 0) {
-        $d.innerHTML = "&times;";
-        $h.innerHTML = "&times;";
-        $m.innerHTML = "&times;";
-        $s.innerHTML = "&times;";
-        clearInterval(countdownTimer);
-        return;
-    }
+  var currentDate = new Date().getTime();
+  var secondsLeft = (targetTS - currentDate) / 1000;
+  if (secondsLeft < 0) {
+    $d.innerHTML = "&times;";
+    $h.innerHTML = "&times;";
+    $m.innerHTML = "&times;";
+    $s.innerHTML = "&times;";
+    clearInterval(countdownTimer);
+    return;
+  }
 
-    var days = parseInt(secondsLeft / 86400);
-    secondsLeft = secondsLeft % 86400;
+  var days = parseInt(secondsLeft / 86400);
+  secondsLeft = secondsLeft % 86400;
 
-    var hours = parseInt(secondsLeft / 3600);
-    secondsLeft = secondsLeft % 3600;
+  var hours = parseInt(secondsLeft / 3600);
+  secondsLeft = secondsLeft % 3600;
 
-    var minutes = parseInt(secondsLeft / 60);
-    var seconds = parseInt(secondsLeft % 60);
+  var minutes = parseInt(secondsLeft / 60);
+  var seconds = parseInt(secondsLeft % 60);
 
-    $d.innerHTML = days;
-    $h.innerHTML = hours;
-    $m.innerHTML = minutes;
+  $d.innerHTML = days;
+  $h.innerHTML = hours;
+  $m.innerHTML = minutes;
 
-    var i = seconds % 3;
-    $s.className = $sClasses[i];
-    $sElements[i].innerHTML = seconds;
+  var i = seconds % 3;
+  $s.className = $sClasses[i];
+  $sElements[i].innerHTML = seconds;
 }
 
 updateCountDown();
